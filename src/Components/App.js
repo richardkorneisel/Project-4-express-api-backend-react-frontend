3cd2bb560820ea88ff3dcfe8ef253203a658a346
Rich Korneisel <korneisel@cfu.net>
2021-03-12 13:34:32 -0600

    Installed react, react router, npm, axios, bootstrap, and added axios call working

---
+++ src/Components/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
+import axios from 'axios';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      travels: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('http://localhost:3000/api/travels', {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+      .then(response => {
+        this.setState({
+          travels: response.data
+        })
+      })
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <body>
+        <div>
+          <h1>Home</h1>
+        </div>
+      </body>
+    );
+  }
+}
+
+export default App;

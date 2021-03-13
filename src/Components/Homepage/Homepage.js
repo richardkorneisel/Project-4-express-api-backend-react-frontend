3420d969cab5dee0fead02f79933de0083edb5b3
Todd Templen <trtemplen@gmail.com>
2021-03-12 20:19:27 -0600

    successfully rending and navigating page components, also passing props and rendering data on pages

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Homepage.css';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
 
 export default class Homepage extends Component {
 
@@ -7,6 +8,10 @@ export default class Homepage extends Component {
         return (
             <div className="home">
                 <h3>Homepage</h3>
+                <Link to={'../TripGallery'}>Trip Gallery</Link> <br />
+                <Link to={'../CreateTrip'}>Create Trip</Link> <br />
+                <Link to={'../Instructions'}>Instructions</Link> <br />
+                   
             </div>
         );
     }

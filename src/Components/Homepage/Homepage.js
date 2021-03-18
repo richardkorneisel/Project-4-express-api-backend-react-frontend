f519234677720648fbdf5eda8de80107d6d3ed58
Rich Korneisel <korneisel@cfu.net>
2021-03-17 21:12:50 -0500

    Fixed background image sizing, and other css items.

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -6,12 +6,14 @@ export default class Homepage extends Component {
 
     render() {
         return (
-            <div className="homeImage">
-               <div className="home">
-                <h3>Homepage</h3>
-                
+            <Container className="homeImage">
+                <div className="home">
+                    <div className="welcome">
+                        <h3>Welcome to the Travel Report App</h3>
+                        <h6>A great place to read about the travel destinations you want to go and post about the memorable places you have been.</h6>
+                    </div>
                 </div>
-            </div>
+            </Container>
 
             /* <Jumbotron fluid>
                 <Container>
--- src/Components/Instructions/Instructions.js
+++ src/Components/Instructions/Instructions.js
@@ -7,7 +7,12 @@ export default class Instructions extends Component {
         return (
             <div className="home">
                 <h3>Instructions</h3>
-                <h5>1) adflksdjflkjsdf</h5>
+                <h6>Our App allows you to share the details of a trip on the internet.</h6>
+                <h6>There are some things we are not able to do at this time though such as host pictures on our App.</h6>
+                <h6>We do allow a place for the user to input a URL address for an image hosted on a hosting sight, this allows the user to display an image along with their written description of their trip.</h6> <h6>An example of an image hosting service/website would be Imgur.</h6> 
+                <h6><a href="https://imgur.com/" target="_blank">Imgur.com</a></h6>
+                <h6>Once you create a free account Imgur provides a direct link to your image and that link can be copied and pasted into our "Image URL" field.</h6>
+                <h6>Google Maps also provides a link to share on their web service from which a link can be copied and pasted into the "Map URL" field in the same way.</h6>
             </div>
         );
     }
